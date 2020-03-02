d067d0ba4304b10ff83c22ba505834b4349d8988
Yura <hvalovyura@gmail.com>
2020-03-03 01:31:08 +0300

    Adding action listener (repaint timer) + Frame constructor

--- src/Lab6/Field.java
+++ src/Lab6/Field.java
@@ -1,8 +1,26 @@
 package Lab6;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Field extends JFrame
 {
-    
+    private ArrayList<BouncingBall> balls = new ArrayList<BouncingBall>(10);
+
+    private boolean paused;
+
+    private Timer repaintTimer = new Timer(10, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            repaint();
+        }
+    });
+
+    public Field()
+    {
+        setBackground(Color.WHITE);
+        repaintTimer.start();
+    }
 }
