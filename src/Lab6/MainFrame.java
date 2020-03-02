e1fc28a9db0430ee58d859e5e24382a237fd3826
Yura <hvalovyura@gmail.com>
2020-03-03 01:19:32 +0300

    Adding fields in MainFrame

--- src/Lab6/MainFrame.java
+++ src/Lab6/MainFrame.java
@@ -1,9 +1,19 @@
 package Lab6;
 
+import java.awt.*;
 import javax.swing.*;
 
+@SuppressWarnings("serial")
 public class MainFrame extends JFrame
 {
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 500;
+
+    private JMenuItem pauseMenuItem;
+    private JMenuItem resumeMenuItem;
+
+    private Field field = new Field();
+
     public static void main(String[] args)
     {
         MainFrame frame = new MainFrame();
