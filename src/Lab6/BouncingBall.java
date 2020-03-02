0517b2187ea67e35fc8ab77173ad2a960c0df70a
Yura <hvalovyura@gmail.com>
2020-03-03 02:22:24 +0300

    Adding fields in BouncingBall class

--- src/Lab6/BouncingBall.java
+++ src/Lab6/BouncingBall.java
@@ -5,7 +5,20 @@ import java.awt.geom.Ellipse2D;
 
 public class BouncingBall implements Runnable
 {
-    
+    private static final int MAX_RADIUS = 40;
+    private static final int MIN_RADIUS = 3;
+    private static final int MAX_SPEED = 15;
+
+    private Field field;
+    private int radius;
+    private Color color;
+
+    private double x;
+    private double y;
+
+    private int speed;
+    private double speedX;
+    private double speedY;
 
     public void BouncingBall(Field field)
     {
